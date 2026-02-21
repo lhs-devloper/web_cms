c3d76a7dddc9996d08870ff2fe457916d1d59078
lhs-devloper <sdsd0114@naver.com>
2026-02-21 13:43:38 +0900

    Update source

--- src/main/java/com/lhsdev/cmsproject/controller/admin/AdminSocialController.java
+++ src/main/java/com/lhsdev/cmsproject/controller/admin/AdminSocialController.java
@@ -3,6 +3,7 @@ package com.lhsdev.cmsproject.controller.admin;
 import com.lhsdev.cmsproject.domain.user.SocialServiceConfig;
 import com.lhsdev.cmsproject.service.SocialServiceConfigService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequiredArgsConstructor
 public class AdminSocialController {
 
-    private final SocialServiceConfigService socialService;
+    @Autowired
+    private SocialServiceConfigService socialService;
 
     @GetMapping
     public String list(Model model) {
